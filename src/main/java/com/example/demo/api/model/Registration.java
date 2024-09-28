f19dcb6711e378ad0c2a20fdc5e0bccc12dd8d56
Sapar <saparaidyn3@gmail.com>
2024-09-28 21:33:19 +0500

    API's for e commerce website

---
+++ src/main/java/com/example/demo/api/model/Registration.java
@@ -0,0 +1,30 @@
+package com.example.demo.api.model;
+
+public class Registration {
+
+    private String username;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "short_description", nullable = false)
+    private String shortDescription;
+
+    @Column(name = "long_description")
+    private String longDescription;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @OneToOne(mappedBy = "product", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private Inventory inventory;
+
+    public Inventory getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(Inventory inventory) {
+        this.inventory = inventory;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
